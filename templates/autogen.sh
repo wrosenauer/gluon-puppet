98777956276676680dc5fe1eaf99b8abf009a4fc
Wolfgang Rosenauer <wr@rosenauer.org>
2016-03-20 22:39:55 +0100

    add some cleanup options

--- templates/autogen.sh
+++ templates/autogen.sh
@@ -1,6 +1,17 @@
 #! /bin/sh -xe
 cd /srv/gluon-<%= @community %>
 
+if [ "$1" = "clean" ]; then
+  make GLUON_TARGET="ar71xx-generic" clean
+  make GLUON_TARGET="mpc85xx-generic" clean
+  exit 0
+fi
+
+if [ "$1" = "dirclean" ]; then
+  make dirclean
+  exit 0
+fi
+
 [ -d .git ] || git init
 
 if [ "$1" = "" ]; then
@@ -20,3 +31,4 @@ make GLUON_BRANCH="$branch" GLUON_TARGET="ar71xx-generic"  $*
 make GLUON_BRANCH="$branch" GLUON_TARGET="mpc85xx-generic" $*
 
 ./propagate.sh "$branch"
+
